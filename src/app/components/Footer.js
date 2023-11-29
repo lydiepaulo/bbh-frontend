7bd6fb83ad9b9756140d0c92f345b70184455f89
Lydie Paulo <lydiepaulo.ui@gmail.com>
2023-11-29 04:10:09 +0100

    Footer links

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -5,29 +5,29 @@ export default function Footer() {
     return (
         <div className="z-10 footer flex flex-col w-screen justify-center items-center gap-[52px] py-[124px]">
             <div className="flex gap-[56px]">
-                <a href="">
-                    <img src="/images/fb.svg" alt="" />
+                <a href="https://www.facebook.com/BBHOfficiel" target="_blank">
+                    <img src="/images/fb.svg" alt="Logo facebook" />
                 </a>
-                <a href="">
-                    <img src="/images/x.svg" alt="" />
+                <a href="https://twitter.com/i/flow/login?redirect_after_login=%2FBBH_Officiel" target="_blank">
+                    <img src="/images/x.svg" alt="Logo X" />
                 </a>
-                <a href="">
-                    <img src="/images/insta.svg" alt="" />
+                <a href="https://www.instagram.com/bbh_officiel/?hl=fr" target="_blank">
+                    <img src="/images/insta.svg" alt="Logo Instagram" />
                 </a>
-                <a href="">
-                    <img src="/images/lkdn.svg" alt="" />
+                <a href="https://www.linkedin.com/company/bbh-business/?originalSubdomain=fr" target="_blank">
+                    <img src="/images/lkdn.svg" alt="Logo Linkedin" />
                 </a>
-                <a href="">
-                    <img src="/images/tiktok.svg" alt="" />
+                <a href="https://www.tiktok.com/@bbhofficiel" target="_blank">
+                    <img src="/images/tiktok.svg" alt="Logo Tiktok" />
                 </a>
-                <a href="">
-                    <img src="/images/yt.svg" alt="" />
+                <a href="https://www.youtube.com/c/brestbretagnehandballtv" target="_blank">
+                    <img src="/images/yt.svg" alt="Logo Youtube" />
                 </a>
             </div>
-            <a href="">Lumy.</a>
+            <a href="https://lumy.bzh/" target="_blank">Lumy.</a>
             <div className="flex gap-4">
-                <a href="">Mentions légales</a>
-                <a href="">Site officiel</a>
+                <a href="" className="font-normal text-white/50">Mentions légales</a>
+                <a href="" className="font-normal text-white/50">Site officiel</a>
             </div>
         </div>
     )
